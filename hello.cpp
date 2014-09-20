b3e83bb529417f57436e5be6e75d234490fa64de
QilongZhang <speknight4534@gmail.com>
2014-09-20 08:53:27 +0800

    hello,world

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: hello.cpp
+	> Author: QilongZhang
+	> Mail: Speknight4534@gmail.com
+	> Created Time: 2014年09月20日 星期六 08时45分09秒
+ ************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"Hello, World!"<<endl;
+    return 0;
+}
