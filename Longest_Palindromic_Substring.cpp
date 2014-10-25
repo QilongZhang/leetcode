589aaf99630eda412e23f8599eea8e0d8bd799fe
QilongZhang <speknight4534@gmail.com>
2014-10-25 16:29:00 +0800

    imprved

--- Longest_Palindromic_Substring.cpp
+++ Longest_Palindromic_Substring.cpp
@@ -14,6 +14,7 @@ class Solution{
 public:
     string longestPalindrome(string s){
         bool dp[1000][1000];
+        int st=0, len=0;
         for(int i=0; i<s.size(); ++i){
             dp[i][i] = true;
             for(int j=i+1; j<s.size(); ++j)
@@ -26,22 +27,12 @@ public:
                         dp[n][m] = true;
                     else if(dp[n+1][m-1])
                         dp[n][m] = true;
+                    if(dp[n][m] && m-n>len){
+                        len = m-n;
+                        st = n;
+                    }
                 }
             }
-        }
-        int len = -1, st = -1;
-        for(int p=0; p<s.size(); ++p){
-            for(int q=s.size()-1; q>=p; --q){
-                if(q-p<len)
-                    break;
-                if(dp[p][q]){
-                    if(q-p>len){
-                        st=p;
-                        len=q-p;
-                    }
-                    break;
-                }
-            }    
         }
         return s.substr(st, len+1);
     }   
