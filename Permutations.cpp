4de88521960d1653cd11d5c838a0d9d05998201a
QilongZhang <speknight4534@gmail.com>
2014-11-05 17:06:27 +0800

    Time: O(n * m\!), Space: O(n * m\!)

--- Permutations.cpp
+++ Permutations.cpp
@@ -7,26 +7,37 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 class Solution{
 public:
-    vector<vector<int> > permute(vector<int> &num){
+    vector<vector<int> > permuteUnique(vector<int> &num){
+        sort(num.begin(), num.end());
         vector<vector<int> > vvi;
         vvi.push_back(num);
         int ed = 0;
         for(int i = 1; i < num.size(); ++i){
             for(int j = 0; j < i; ++j){
+                if(num[i] == num[j])
+                    continue;
+                if(j != i - 1 && num[j] == num[j + 1])
+                    continue;
+                int pre = vvi.size() - 1, n; 
                 for(int k = 0; k <= ed; ++k){
                     vector<int> vi = vvi[k];
                     vi[i] = num[j];
-                    for(int m = 0; m < vi.size(); ++m)
+                    for(int m = 0; m < i; ++m)
                         if(vi[m] == num[j]){
                             vi[m] = num[i];
-                            break;
+                            for(n = pre; n < vvi.size(); ++n)
+                                if(vi == vvi[n])
+                                    break;
+                            if(n == vvi.size())
+                                vvi.push_back(vi);
+                            vi[m] = num[j];
                         }
-                    vvi.push_back(vi);
                 }
             }
             ed = vvi.size() - 1;
@@ -35,3 +46,15 @@ public:
     }
 };
 
+int main(){
+    vector<int> vi(4);
+    vi[0] = 2, vi[1] = 1, vi[2] = 1, vi[3] = 2;
+    Solution st;
+    vector<vector<int> > vvi = st.permute(vi);
+    for(int i = 0; i < vvi.size(); ++i){
+        for(int j = 0; j < vvi[i].size(); ++j)
+            cout<<vvi[i][j]<<' ';
+        cout<<endl;
+    }
+    return 0;
+}
